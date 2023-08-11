088813ca2c6369256c1ce6b796051aa8568bef92
Neelesh Saxena <saxenaneelo133@gmail.com>
2023-08-12 00:48:30 +0530

    Registering new user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,50 @@
-const registerUser = async (req, res) => {
-  res.send("Register User");
-};
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+User;
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  //Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill in all required fields");
+  }
+  if ((password.length < 6)) {
+    res.status(400);
+    throw new Error("Password must be minimum 6 characters");
+  }
+
+  //Check if user email already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("Email has already registered");
+  }
+
+  //Create new user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(201).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+    });
+  }
+  else{
+    res.status(400)
+    throw new Error("Invalid user data")
+  }
+});
 
 module.exports = { registerUser };
