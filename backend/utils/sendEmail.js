23ed8511b46f2190a65d8659ad148722cf5b3b52
Neelesh Saxena <saxenaneelo133@gmail.com>
2023-08-13 01:13:10 +0530

    Function to send mail

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,37 @@
+const nodeMailer = require('nodemailer')
+
+const sendEmail = asyncHandler(async(subject,message,send_to,sent_from,reply_to) =>{
+    //Create email transporter
+    const transporter= nodeMailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: 587,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    })
+
+    //Option for sending email
+    const options = {
+        from: sent_from,
+        to: send_to,
+        reply_to: reply_to,
+        subject: subject,
+        html: message,
+    }
+
+    //Send email
+    transporter.sendMail(options, function(err,info){
+        if(err){
+            console.log(err)
+        }
+        else{
+            console.log(info);
+        }
+    })
+});
+
+module.exports = sendEmail
