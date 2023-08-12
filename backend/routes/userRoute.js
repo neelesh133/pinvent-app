a030bce8a51d0d7decdf332b0cce519094d64812
Neelesh Saxena <saxenaneelo133@gmail.com>
2023-08-12 16:21:10 +0530

    Creating update user details route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser,loginUser,logoutUser,getUser,loginStatus } = require("../controllers/userController");
+const { registerUser,loginUser,logoutUser,getUser,loginStatus,updateUser } = require("../controllers/userController");
 const protect = require("../middleware/authMiddleware");
 
 router.post("/register", registerUser);
@@ -8,6 +8,7 @@ router.post("/login", loginUser);
 router.get("/logout", logoutUser);
 router.get("/getuser",protect, getUser);
 router.get("/loggedin", loginStatus);
+router.patch("/updateuser",protect, updateUser);
 
 module.exports = router;
 
