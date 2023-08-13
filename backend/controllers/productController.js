7a515a850e370dc25788824f738c43d8a2a50079
Neelesh Saxena <saxenaneelo133@gmail.com>
2023-08-13 13:41:01 +0530

    Created product route (Image not added)

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,28 @@
 const expressAsyncHandler = require("express-async-handler");
+const Product = require("../models/productModel");
+Product;
 
-const createProduct = expressAsyncHandler(async(req,res) =>{
-    res.send("hello")
+const createProduct = expressAsyncHandler(async (req, res) => {
+  const { name, sku, category, quantity, price, description } = req.body;
+
+  //Validation
+  if (!name || !category || !quantity || !price || !description) {
+    res.status(400);
+    throw new Error("Please fill in all fields");
+  }
+
+  //create product
+  const product = await Product.create({
+    user: req.user._id,
+    name,
+    sku,
+    category,
+    quantity,
+    price,
+    description,
+  });
+
+  res.status(201).json(product)
 });
 
-module.exports = {createProduct}
+module.exports = { createProduct };
