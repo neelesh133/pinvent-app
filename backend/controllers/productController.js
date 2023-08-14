bed0f6ba1497297316b85c28a76fd9c8052bce67
Neelesh Saxena <saxenaneelo133@gmail.com>
2023-08-14 19:21:53 +0530

    Saving image on cloudinary

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@ const expressAsyncHandler = require("express-async-handler");
 const Product = require("../models/productModel");
 const { upload, fileSizeFormatter } = require("../utils/fileUpload");
 Product;
+const cloudinary = require("cloudinary").v2;
 
 const createProduct = expressAsyncHandler(async (req, res) => {
   const { name, sku, category, quantity, price, description } = req.body;
@@ -13,14 +14,26 @@ const createProduct = expressAsyncHandler(async (req, res) => {
   }
 
   //Handle image upload
-  let fileData = {}
-  if(req.file) {
-    fileData = {
-        fileName: req.file.originalname,
-        filePath: req.file.path,
-        fileType: req.file.mimetype,
-        fileSize: fileSizeFormatter(req.file.size, 2),
+  let fileData = {};
+  if (req.file) {
+    //Save image to cloudinary
+    let uploadedFile;
+    try {
+      uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+        folder: "Pinvent App",
+        resource_type: "image",
+      });
+    } catch (error) {
+      res.status(500);
+      throw new Error("Image could not be uploaded");
     }
+
+    fileData = {
+      fileName: req.file.originalname,
+      filePath: uploadedFile.secure_url,
+      fileType: req.file.mimetype,
+      fileSize: fileSizeFormatter(req.file.size, 2),
+    };
   }
 
   //create product
@@ -32,10 +45,12 @@ const createProduct = expressAsyncHandler(async (req, res) => {
     quantity,
     price,
     description,
-    image: fileData
+    image: fileData,
   });
 
-  res.status(201).json(product)
+  res.status(201).json(product);
 });
 
+//Get all Products
+// const getProducts = asyncHandler(async (req, res) => {});
 module.exports = { createProduct };
