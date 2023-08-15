457aa72174e15d8ea448e4c3783699b42e101374
Neelesh Saxena <saxenaneelo133@gmail.com>
2023-08-15 18:47:33 +0530

    Update  product route created

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -52,9 +52,7 @@ const createProduct = asyncHandler(async (req, res) => {
 
 //Get all Products
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({ user: req.user.id }).sort(
-    "-createdAt"
-  );
+  const products = await Product.find({ user: req.user.id }).sort("-createdAt");
   res.status(200).json(products);
 });
 
@@ -75,7 +73,7 @@ const getProduct = asyncHandler(async (req, res) => {
 });
 
 //Delete product
-const deleteProduct = asyncHandler(async(req,res) =>{
+const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
@@ -89,7 +87,70 @@ const deleteProduct = asyncHandler(async(req,res) =>{
     throw new Error("User not authorized");
   }
 
-  await product.deleteOne({_id: req.params.id})
-  res.status(200).json({message:"Product deleted successfully",product});
+  await product.deleteOne({ _id: req.params.id });
+  res.status(200).json({ message: "Product deleted successfully", product });
 });
-module.exports = { createProduct, getProducts, getProduct, deleteProduct };
+
+//Update Product
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, category, quantity, price, description } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+
+  //Match product to its user
+  if (product.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  //Handle image upload
+  let fileData = {};
+  if (req.file) {
+    //Save image to cloudinary
+    let uploadedFile;
+    try {
+      uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+        folder: "Pinvent App",
+        resource_type: "image",
+      });
+    } catch (error) {
+      res.status(500);
+      throw new Error("Image could not be uploaded");
+    }
+
+    fileData = {
+      fileName: req.file.originalname,
+      filePath: uploadedFile.secure_url,
+      fileType: req.file.mimetype,
+      fileSize: fileSizeFormatter(req.file.size, 2),
+    };
+  }
+
+  //update product
+  const updatedProduct = await Product.findByIdAndUpdate({_id: req.params.id},
+    {
+    name,
+    category,
+    quantity,
+    price,
+    description,
+    image: Object.keys(fileData).length === 0 ? product.image : fileData,
+    },{
+      new: true,
+      runValidators: true
+    })
+
+  res.status(200).json(updatedProduct);
+});
+module.exports = {
+  createProduct,
+  getProducts,
+  getProduct,
+  deleteProduct,
+  updateProduct,
+};
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createProduct, getProducts, getProduct, deleteProduct } = require("../controllers/productController");
+const { createProduct, getProducts, getProduct, deleteProduct, updateProduct } = require("../controllers/productController");
 const protect = require("../middleware/authMiddleware");
 const { upload } = require("../utils/fileUpload");
 
@@ -9,5 +9,6 @@ router.post("/",protect,upload.single("image"),createProduct);
 router.get("/getproducts",protect,getProducts);
 router.get("/getproduct/:id",protect,getProduct);
 router.delete("/deleteproduct/:id",protect,deleteProduct);
+router.patch("/updateproduct/:id",upload.single("image"),protect,updateProduct);
 
 module.exports = router;
